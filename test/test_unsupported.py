2516169386de4c4b1b06b11a31e09b6add60c2aa
Lars Hummelgren <larshum@kth.se>
2024-12-15 14:17:26 +0100

    Test that unsupported Python features are rejected

---
+++ test/test_unsupported.py
@@ -0,0 +1,35 @@
+import parir
+import pytest
+import torch
+
+def assert_runtime_error_on_jit(fn):
+    with pytest.raises(RuntimeError):
+        parir.jit(fn)
+
+def add_slice(x, y, out, N):
+    out[:N] = x[:N] + y[:N]
+def test_slicing():
+    assert_runtime_error_on_jit(add_slice)
+
+def while_fun(x, y, N):
+    i = 0
+    while i < N:
+        y[i] = x[i]
+        i += 1
+def test_while():
+    assert_runtime_error_on_jit(while_fun)
+
+def for_steps(x, y, N):
+    for i in range(0, N, 2):
+        y[i] = x[i]
+        y[i+1] = x[i+1]
+def test_for_steps():
+    assert_runtime_error_on_jit(for_steps)
+
+def for_else(x, y, N):
+    for i in range(N):
+        y[i] = x[i]
+    else:
+        y[0] += 1
+def test_for_else():
+    assert_runtime_error_on_jit(for_else)
