1ca6b9033ef3ad430a4d17a539b21c923d1f031d
Lars Hummelgren <larshum@kth.se>
2024-12-15 17:36:46 +0100

    Clean up root parir Python file

--- parir/__init__.py
+++ parir/__init__.py
@@ -1,12 +1,30 @@
-import ast as python_ast
-import inspect
-import torch.utils.cpp_extension
-
 from . import key, parir
+
+from pathlib import Path
+import os
+cache_path = Path(f"{os.path.expanduser('~')}/.cache/parir")
+cache_path.mkdir(parents=True, exist_ok=True)
 
 compiled_cache = {}
 
-def compile_function(ir_ast, args, kwargs, fn):
+def compile_to_binary(cpp, cu_host, cu_dev, name):
+    # Use Torch to produce binary code from the output code
+    import torch.utils.cpp_extension
+    if len(cu_dev) == 0:
+        cpp_sources = [f"{cu_host}\n{cpp}"]
+        cuda_sources = []
+    else:
+        cpp_sources = [cpp]
+        cuda_sources = [f"{cu_dev}\n{cu_host}"]
+    module = torch.utils.cpp_extension.load_inline(
+        name = name,
+        cpp_sources = cpp_sources,
+        cuda_sources = cuda_sources
+    )
+
+    return getattr(module, name)
+
+def compile_function(ir_ast, args, kwargs, fn, key):
     # Look for the parallelize keyword argument, which is used to control how
     # the for-loops of the function should be parallelized. If it is not
     # specified, we return the original function without performing any extra
@@ -25,35 +43,29 @@ def compile_function(ir_ast, args, kwargs, fn):
     # low-level code.
     cpp, cu_host, cu_dev = parir.compile_ir(ir_ast, args, par)
 
-    # Use Torch to produce binary code from the output code
-    if len(cu_dev) == 0:
-        cpp_sources = [f"{cu_host}\n{cpp}"]
-        cuda_sources = []
-    else:
-        cpp_sources = [cpp]
-        cuda_sources = [f"{cu_dev}\n{cu_host}"]
-    module = torch.utils.cpp_extension.load_inline(
-        name = fn.__name__,
-        cpp_sources = cpp_sources,
-        cuda_sources = cuda_sources
-    )
+    return compile_to_binary(cpp, cu_host, cu_dev, fn.__name__)
 
-    return getattr(module, fn.__name__)
-
-def jit(fn):
-    # When we first reach a function, we compile it to the IR AST with little
-    # type information. Then, when the function is actually called, we use the
-    # provided function arguments and the parallelization arguments to produce
-    # a complete IR AST before compiling this to low-level code.
+def convert_python_function_to_ir(fn):
+    import ast as python_ast
+    import inspect
     filepath = inspect.getfile(fn)
     src, fst_line = inspect.getsourcelines(fn)
     ast = python_ast.parse("".join(src))
-    ir_ast = parir.python_to_ir(ast, filepath, fst_line-1)
+    return parir.python_to_ir(ast, filepath, fst_line-1)
+
+def jit(fn):
+    """
+    Prepares the provided function for JIT-compilation. Initially, the Python
+    function is parsed and translated to an untyped IR AST once. Then, for each
+    time the function is used, the IR AST is JIT compiled based on the types of
+    the provided arguments.
+    """
+    ir_ast = convert_python_function_to_ir(fn)
 
     def inner(*args, **kwargs):
         k = key.generate_function_key(fn, args, kwargs)
         if not k in compiled_cache:
-            compiled_cache[k] = compile_function(ir_ast, args, kwargs, fn)
+            compiled_cache[k] = compile_function(ir_ast, args, kwargs, fn, key)
         compiled_cache[k](*args)
     inner.__name__ = fn.__name__
     return inner
