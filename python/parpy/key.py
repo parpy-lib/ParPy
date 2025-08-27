7abd3723fa6569f64cb80a188fbf592b942f764c
Lars Hummelgren <larshum@kth.se>
2025-08-27 14:08:31 +0200

    Fix missed renames

--- python/parpy/key.py
+++ python/parpy/key.py
@@ -11,7 +11,7 @@ def _print_argument_key(arg):
     if isinstance(arg, dict):
         s = []
         for k, v in sorted(arg.items()):
-            s.append(f"{k}: {print_argument_key(v)}")
+            s.append(f"{k}: {_print_argument_key(v)}")
         return """{{{", ".join(s)}}}"""
     elif isinstance(arg, Buffer):
         if len(arg.shape) == 0:
@@ -31,7 +31,7 @@ def _print_compile_options_key(opts):
 
 def _generate_fast_cache_key(ir_ast, args, opts):
     from .parpy import print_ast
-    ir_key = _print_ast(ir_ast)
+    ir_key = print_ast(ir_ast)
     args_key = _print_arguments_key(args)
     opts_key = _print_compile_options_key(opts)
     return f"{ir_key}+{args_key}+{opts_key}"
