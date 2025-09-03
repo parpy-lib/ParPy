d86cbb8122fd354d1243531ca5e61addd9e9fb32
Lars Hummelgren <larshum@kth.se>
2025-09-03 10:48:08 +0200

    Update JIT compilation tutorial example

--- examples/jit-compiling.py
+++ examples/jit-compiling.py
@@ -1,11 +1,14 @@
+# Initial version using lists
 def mv(A, b):
     return [sum(A_val * b_val for (A_val, b_val) in zip(A_row, b)) for A_row in A]
 
 A = [[2.5, 3.5], [1.5, 0.5]]
 b = [2.0, 1.0]
-print(mv(A, b))
+out = mv(A, b)
+print("Lists", out)
 
-def mv(A, b, out, N):
+# Using in-place mutation instead of returning a value
+def mv_inplace(A, b, out, N):
     for row in range(N):
         out[row] = sum(A_val * b_val for (A_val, b_val) in zip(A[row], b))
 
@@ -13,27 +16,29 @@ N = 2
 A = [[2.5, 3.5], [1.5, 0.5]]
 b = [2.0, 1.0]
 out = [0.0 for row in range(N)]
-mv(A, b, out, N)
-print(out)
+mv_inplace(A, b, out, N)
+print("Lists (in-place)", out)
 
+# Using NumPy arrays instead of Python lists
 import numpy as np
 
-def mv(A, b, out, N):
+def mv_numpy(A, b, out, N):
     for row in range(N):
         out[row] = sum(A[row] * b)
 
 N = 2
+M = 2
 A = np.array([[2.5, 3.5], [1.5, 0.5]])
 b = np.array([2.0, 1.0])
 out = np.zeros((N,))
-mv(A, b, out, N)
-print(out)
+mv_numpy(A, b, out, N)
+print("NumPy v2", out)
 
-
+# Using parallelization with ParPy
 import parpy
 
 @parpy.jit
-def mv(A, b, out, N):
+def mv_parpy(A, b, out, N):
     parpy.label('N')
     for row in range(N):
         parpy.label('M')
@@ -41,5 +46,19 @@ def mv(A, b, out, N):
 
 out = np.zeros((N,))
 opts = parpy.par({'N': parpy.threads(N)})
-mv(A, b, out, N, opts=opts)
-print(out)
+mv_parpy(A, b, out, N, opts=opts)
+print("ParPy", out)
+
+# ParPy with pre-allocated data
+backend = parpy.CompileBackend.Cuda
+A = parpy.buffer.from_array(A, backend)
+b = parpy.buffer.from_array(b, backend)
+out = parpy.buffer.from_array(out, backend)
+
+import time
+t1 = time.time_ns()
+mv_parpy(A, b, out, N, opts=opts)
+out.sync()
+t2 = time.time_ns()
+print("Time:", (t2-t1)/1e9)
+print("ParPy v2", out.numpy())
