39d5024303324c2341741200a43047da01a2939e
Lars Hummelgren <larshum@kth.se>
2025-12-18 13:34:49 +0100

    Update version to 0.4.0

--- python/parpy/__init__.py
+++ python/parpy/__init__.py
@@ -11,4 +11,4 @@ from .buffer import sync
 from .main import threads, par_reduction, unroll, clear_cache, compile_string, print_compiled, callback, external, jit
 from .builtin import gpu, label
 
-__version__ = "0.3.5"
+__version__ = "0.4.0"
