ca3cd073d22fcdc873add7b510e204374f7e6e51
Lars Hummelgren <larshum@kth.se>
2026-02-17 20:51:47 +0100

    Add native implementation of Any reduce in Triton

--- python/parpy/native/parpy_triton.py
+++ python/parpy/native/parpy_triton.py
@@ -5,6 +5,10 @@ def _parpy_builtin_to_torch(t):
     return t.torch()
 
 @triton.jit
+def _parpy_builtin_any(x):
+    return tl.max(x) == True
+
+@triton.jit
 def _parpy_builtin_mul(x, y):
     return x * y
 
